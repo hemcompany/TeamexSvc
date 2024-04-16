f032e7ea2433e4b808e7d81265a17cc0a0046718
Hem <hmjung@teamexusa.com>
2024-04-16 03:53:43 -0400

    workspace change

---
+++ front/svc/src/App.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import { ThemeProvider, createTheme } from '@mui/material/styles'; 
+import { Fab } from '@mui/material';
+import MorningIcon from '@mui/icons-material/WbSunny';
+import NightIcon from '@mui/icons-material/Brightness3';
+
+function App() {
+  const [dark, setDark] = React.useState(false);
+  const [theme, setTheme] = React.useState(createTheme());
+  const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#1976d2',
+        },
+    },
+  });
+
+  const defaultTheme = createTheme();
+  const ThemeChange = () => {
+    if (dark) {
+      setTheme(defaultTheme);
+    } else {
+      setTheme(darkTheme);
+    }
+    setDark(!dark);
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </Router>
+        <Fab onClick={() => ThemeChange()} color="primary" aria-label="add" style={{ position: 'fixed', bottom: 16, right: 16 }}>
+          {dark ? <MorningIcon /> : <NightIcon /> }
+        </Fab>
+      </ThemeProvider>
+    </>
+  );
+}
+ 
+export default App;
